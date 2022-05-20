211e4b51e58557c548fbe1d60143cf6d7d1dec2c
ullisNest <morsello.1851339@studenti.uniroma1.it>
2022-05-20 17:34:37 +0200

    commit iniziale

---
+++ javafx2/src/javafx2/HelloWorld.java
@@ -0,0 +1,30 @@
+package javafx2;
+
+import javafx.embed.swt.*;
+import javafx.*;
+import javafx.embed.*;
+ 
+public class HelloWorld extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
